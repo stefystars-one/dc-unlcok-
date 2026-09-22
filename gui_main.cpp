dcefed17d81b4be2c5b8924e90ba87b0c3c75b50
stefystars-one <stefystars@github.com>
2026-09-22 17:54:43 -0300

    v8.6: Set CURRENT_VERSION=8.6, update rc files, update manifest and sha256

--- gui_main.cpp
+++ gui_main.cpp
@@ -17574,7 +17574,7 @@ const char EMBEDDED_OVERLAY_HTML[] = R"raw_overlay_html(
 using namespace Microsoft::WRL;
 namespace fs = std::filesystem;
 
-const std::string CURRENT_VERSION = "8.5";
+const std::string CURRENT_VERSION = "8.6";
 const std::wstring CLOUD_API_HOST = L"discord-unlock-api.st4rs.workers.dev";
 const std::wstring THEMES_CATALOG_HOST = L"script.google.com";
 const std::wstring THEMES_CATALOG_PATH = L"/macros/s/AKfycbxJeT0t6WzljXxQH5FoyBhQkNad8oQWm7Wzf0aa40oh2fAO3XriJJWHmps3bLAtbpJgdA/exec";
